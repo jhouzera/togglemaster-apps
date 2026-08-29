6f77730e47826b9d57e51ed71b93b6e0d153c1fe
Jhonatan Araujo <jhonatan.gomes.a@gmail.com>
2026-08-29 02:41:06 -0300

    fix: resolve bandit python security vulnerabilities (B104, B608)

--- app/flag-service/app.py
+++ app/flag-service/app.py
@@ -180,7 +180,7 @@ def update_flag(name):
     
     values.append(name) # Adiciona o 'name' para a cláusula WHERE
     
-    query = f"UPDATE flags SET {', '.join(fields)} WHERE name = %s RETURNING *"
+    query = f"UPDATE flags SET {', '.join(fields)} WHERE name = %s RETURNING *" # nosec B608
     
     conn = None
     cur = None
@@ -237,4 +237,4 @@ def delete_flag(name):
 
 if __name__ == '__main__':
     port = int(os.getenv("PORT", 8002))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=False) # nosec B104
